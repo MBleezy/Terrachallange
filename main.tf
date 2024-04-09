3f9e8bc2f08b404757cca1edd7798cc704051a15
Matt Bleezarde <matt.bleezarde@coretek.com>
2024-04-09 15:23:25 -0400

    Final CP1 version

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
 
-resource "azurerm_resource_group" "mbleezarde-sandbox" {
+data "azurerm_resource_group" "mbleezarde-sandbox" {
 	name = "mbleezarde-sandbox"
-	location = "East US"
-	
 	}
 
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
 }
 
 # Create Web subnet
 resource "azurerm_subnet" "terraform_Web_subnet" {
   name                 = "${random_pet.prefix.id}-Web-subnet"
-  resource_group_name  = azurerm_resource_group.mbleezarde-sandbox.name
+  resource_group_name  = data.azurerm_resource_group.mbleezarde-sandbox.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -24,7 +22,7 @@ resource "azurerm_subnet" "terraform_Web_subnet" {
 # Create Data subnet
 resource "azurerm_subnet" "terraform_Data_subnet" {
   name                 = "${random_pet.prefix.id}-Data-subnet"
-  resource_group_name  = azurerm_resource_group.mbleezarde-sandbox.name
+  resource_group_name  = data.azurerm_resource_group.mbleezarde-sandbox.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -32,7 +30,7 @@ resource "azurerm_subnet" "terraform_Data_subnet" {
 # Create Jumpbox subnet
 resource "azurerm_subnet" "terraform_Jump_subnet" {
   name                 = "${random_pet.prefix.id}-Jumpbox-subnet"
-  resource_group_name  = azurerm_resource_group.mbleezarde-sandbox.name
+  resource_group_name  = data.azurerm_resource_group.mbleezarde-sandbox.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.3.0/24"]
 }
@@ -40,23 +38,23 @@ resource "azurerm_subnet" "terraform_Jump_subnet" {
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "${random_pet.prefix.id}-public-ip"
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_public_ip" "my_terraform_Linux_public_ip" {
-  name                = "${random_pet.prefix.id}-public-ip"
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  name                = "${random_pet.prefix.id}-linux-public-ip"
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
   allocation_method   = "Dynamic"
 }
 
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
   name                = "${random_pet.prefix.id}-nsg"
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
 
   security_rule {
     name                       = "RDP"
@@ -85,8 +83,8 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
   name                = "${random_pet.prefix.id}-nic"
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
@@ -97,9 +95,9 @@ resource "azurerm_network_interface" "my_terraform_nic" {
 }
 
 resource "azurerm_network_interface" "my_terraform_linux_nic" {
-  name                = "${random_pet.prefix.id}-nic"
-  location            = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = azurerm_resource_group.mbleezarde-sandbox.name
+  name                = "${random_pet.prefix.id}-linux-nic"
+  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
@@ -124,8 +122,8 @@ resource "azurerm_network_interface_security_group_association" "Linux" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name      = azurerm_resource_group.mbleezarde-sandbox.name
+  location                 = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name      = data.azurerm_resource_group.mbleezarde-sandbox.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -136,8 +134,8 @@ resource "azurerm_windows_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   admin_username        = "azureuser"
   admin_password        = random_password.password.result
-  location              = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name   = azurerm_resource_group.mbleezarde-sandbox.name
+  location              = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name   = data.azurerm_resource_group.mbleezarde-sandbox.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
 
@@ -160,11 +158,11 @@ resource "azurerm_windows_virtual_machine" "main" {
   }
 }
 
-# Create virtual machine
+# Create Linux virtual machine
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   name                  = "myVM"
-  location              = azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name   = azurerm_resource_group.mbleezarde-sandbox.name
+  location              = data.azurerm_resource_group.mbleezarde-sandbox.location
+  resource_group_name   = data.azurerm_resource_group.mbleezarde-sandbox.name
   network_interface_ids = [azurerm_network_interface.my_terraform_linux_nic.id]
   size                  = "Standard_DS1_v2"
 
@@ -198,7 +196,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
 resource "random_id" "random_id" {
   keepers = {
     # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.mbleezarde-sandbox.name
+    resource_group = data.azurerm_resource_group.mbleezarde-sandbox.name
   }
 
   byte_length = 8
