a8b0db14b5c1cf2f9fcd0250b29fa58db1bee29e
Matt Bleezarde <matt.bleezarde@coretek.com>
2024-07-23 15:14:35 -0400

    added key vault

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 data "azurerm_resource_group" "mbleezarde-sandbox" {
-	name = "mbleezarde-sandbox"
-	}
+  name = "mbleezarde-sandbox"
+}
 
 # Create virtual network
 resource "azurerm_virtual_network" "terra_chal_network" {
@@ -128,12 +128,94 @@ resource "azurerm_storage_account" "Terra_chal_SA" {
   account_replication_type = "LRS"
 }
 
+#Create Azure Key Vault
+data "azurerm_client_config" "current" {}
+
+resource "random_string" "azurerm_key_vault_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+locals {
+  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                       = coalesce(var.key_vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
+  location                   = var.resource_group_location
+  resource_group_name        = data.azurerm_resource_group.mbleezarde-sandbox.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = local.current_user_id
+
+    key_permissions    = var.key_permissions
+    secret_permissions = var.secret_permissions
+  }
+}
+
+resource "random_string" "azurerm_key_vault_key_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_key_vault_key" "key" {
+  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
+
+  key_vault_id = azurerm_key_vault.vault.id
+  key_type     = var.key_type
+  key_size     = var.key_size
+  key_opts     = var.key_ops
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
+}
+
+#Create secrets to be used as vm passwords and store in key vault
+resource "random_password" "linux_vault_pw" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&,."
+}
+
+resource "random_password" "windows_vault_pw" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&,."
+}
+
+resource "azurerm_key_vault_secret" "linux_vm_secret" {
+  key_vault_id = azurerm_key_vault.vault.id
+  name         = var.linux_secret
+  value        = random_password.linux_vault_pw.result
+}
+
+resource "azurerm_key_vault_secret" "windows_vm_secret" {
+  key_vault_id = azurerm_key_vault.vault.id
+  name         = var.windows_secret
+  value        = random_password.windows_vault_pw.result
+}
 
 # Create virtual machine
 resource "azurerm_windows_virtual_machine" "Terra_chal_win_vm" {
   name                  = "${var.prefix}vm"
   admin_username        = "azureuser"
-  admin_password        = random_password.password.result
+  admin_password        = azurerm_key_vault_secret.windows_vm_secret.value
   location              = data.azurerm_resource_group.mbleezarde-sandbox.location
   resource_group_name   = data.azurerm_resource_group.mbleezarde-sandbox.name
   network_interface_ids = [azurerm_network_interface.terra_chal_win_nic.id]
@@ -181,6 +263,7 @@ resource "azurerm_linux_virtual_machine" "terra_chal_linux_vm" {
 
   computer_name  = "hostname"
   admin_username = var.username
+  admin_password = azurerm_key_vault_secret.linux_vm_secret.value
 
   admin_ssh_key {
     username   = var.username
@@ -212,53 +295,53 @@ resource "random_password" "password" {
 }
 
 #Create Azure Recovery Services Vault and Backup Policy
-resource "azurerm_recovery_services_vault" "terra_chal_vault" {
-  name                = "${var.prefix}vault"
-  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
-  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
-  sku                 = "Standard"
-  storage_mode_type   = var.storage_mode_type
-  soft_delete_enabled = true
-}
+#resource "azurerm_recovery_services_vault" "terra_chal_vault" {
+#  name                = "${var.prefix}vault"
+#  location            = data.azurerm_resource_group.mbleezarde-sandbox.location
+#  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
+#  sku                 = "Standard"
+#  storage_mode_type   = var.storage_mode_type
+#  soft_delete_enabled = true
+#}
+#
+#resource "azurerm_backup_policy_vm" "terra_vm_backup_policy" {
+#  name                = var.vm_backup_policy_name
+#  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
+#  recovery_vault_name = azurerm_recovery_services_vault.terra_chal_vault.name
+#
+#  timezone = "UTC"
+#
+#  backup {
+#    frequency = var.vm_backup_policy_frequency
+#    time      = var.vm_backup_policy_time
+#  }
+#
+#  retention_daily {
+#    count = 10
+#  }
+#
+#  retention_weekly {
+#    count    = 42
+#    weekdays = ["Sunday", "Wednesday", "Friday", "Saturday"]
+#  }
+#
+#  retention_monthly {
+#    count    = 7
+#    weekdays = ["Sunday", "Wednesday"]
+#    weeks    = ["First", "Last"]
+#  }
+#
+#  retention_yearly {
+#    count    = 77
+#    weekdays = ["Sunday"]
+#    weeks    = ["Last"]
+#    months   = ["January"]
+#  }
+#}
 
-resource "azurerm_backup_policy_vm" "terra_vm_backup_policy" {
-  name                = var.vm_backup_policy_name
-  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
-  recovery_vault_name = azurerm_recovery_services_vault.terra_chal_vault.name
-
-  timezone = "UTC"
-
-  backup {
-    frequency = var.vm_backup_policy_frequency
-    time      = var.vm_backup_policy_time
-  }
-
-  retention_daily {
-    count = 10
-  }
-
-    retention_weekly {
-    count    = 42
-    weekdays = ["Sunday", "Wednesday", "Friday", "Saturday"]
-  }
-
-  retention_monthly {
-    count    = 7
-    weekdays = ["Sunday", "Wednesday"]
-    weeks    = ["First", "Last"]
-  }
-
-  retention_yearly {
-    count    = 77
-    weekdays = ["Sunday"]
-    weeks    = ["Last"]
-    months   = ["January"]
-  }
-}
-
-resource "azurerm_backup_protected_vm" "terra_protected_vm" {
-    resource_group_name             = data.azurerm_resource_group.mbleezarde-sandbox.name
-    recovery_vault_name             = azurerm_recovery_services_vault.terra_chal_vault.name
-    source_vm_id                    = azurerm_windows_virtual_machine.Terra_chal_win_vm.id
-    backup_policy_id                = azurerm_backup_policy_vm.terra_vm_backup_policy.id
-}
+#resource "azurerm_backup_protected_vm" "terra_protected_vm" {
+#  resource_group_name = data.azurerm_resource_group.mbleezarde-sandbox.name
+#  recovery_vault_name = azurerm_recovery_services_vault.terra_chal_vault.name
+#  source_vm_id        = azurerm_windows_virtual_machine.Terra_chal_win_vm.id
+#  backup_policy_id    = azurerm_backup_policy_vm.terra_vm_backup_policy.id
+#}
