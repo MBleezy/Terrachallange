22d352aa9f18bcd348ec56ce645c158f04f56051
Matt Bleezarde <matt.bleezarde@coretek.com>
2024-07-23 16:44:10 -0400

    Code updates

--- outputs.tf
+++ outputs.tf
@@ -7,15 +7,15 @@ output "admin_password" {
   value     = azurerm_windows_virtual_machine.Terra_chal_win_vm.admin_password
 }
 
-output "recovery_vault_id" {
-  description = "The ID of the Recovery Services Vault."
-  value       = azurerm_recovery_services_vault.terra_chal_vault.id
-}
-
-output "recovery_vault_name" {
-  description = "The name of the Recovery Services Vault."
-  value       = azurerm_recovery_services_vault.terra_chal_vault.name
-}
+#output "recovery_vault_id" {
+#  description = "The ID of the Recovery Services Vault."
+#  value       = azurerm_recovery_services_vault.terra_chal_vault.id
+#}
+#
+#output "recovery_vault_name" {
+#  description = "The name of the Recovery Services Vault."
+#  value       = azurerm_recovery_services_vault.terra_chal_vault.name
+#}
 
 output "azurerm_key_vault_name" {
   value = azurerm_key_vault.vault.name
